ab74658c5d275cb4bb839944e3f6e836028dd54a
송광호 <skh990427@naver.com>
2025-01-22 00:15:17 +0900

    서블릿 필터 - 로그인 인증 체크

--- login/src/main/java/hello/login/WebConfig.java
+++ login/src/main/java/hello/login/WebConfig.java
@@ -1,6 +1,7 @@
 package hello.login;
 
 import hello.login.web.filter.LogFilter;
+import hello.login.web.filter.LoginCheckFilter;
 import jakarta.servlet.Filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
@@ -14,6 +15,16 @@ public class WebConfig {
         FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
         filterFilterRegistrationBean.setFilter(new LogFilter()); // 내가 만든 필터 추가
         filterFilterRegistrationBean.setOrder(1); // 순서
+        filterFilterRegistrationBean.addUrlPatterns("/*"); // URL 패턴에 적용할건지
+
+        return filterFilterRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean loginCheckFilter() {
+        FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
+        filterFilterRegistrationBean.setFilter(new LoginCheckFilter()); // 내가 만든 필터 추가
+        filterFilterRegistrationBean.setOrder(2); // 순서
         filterFilterRegistrationBean.addUrlPatterns("/*"); // URL 패턴에 적용할건지
 
         return filterFilterRegistrationBean;

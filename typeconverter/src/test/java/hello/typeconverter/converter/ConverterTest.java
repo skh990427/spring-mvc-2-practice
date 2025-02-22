50ad5d61c79a30dc7f1d6d47835a918827b4bd3b
송광호 <skh990427@naver.com>
2025-02-22 16:45:56 +0900

    섹션11: 타입 컨버터 - Converter

---
+++ typeconverter/src/test/java/hello/typeconverter/converter/ConverterTest.java
@@ -0,0 +1,45 @@
+package hello.typeconverter.converter;
+
+import hello.typeconverter.type.IpPort;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class ConverterTest {
+
+    @Test
+    @DisplayName("문자열 정수로 변환 컨버터")
+    void stringToInteger() throws Exception {
+        StringToIntegerConverter converter = new StringToIntegerConverter();
+        Integer result = converter.convert("10");
+        assertThat(result).isEqualTo(10);
+    }
+
+    @Test
+    @DisplayName("숫자를 문자열로 변환 컨버터")
+    void integerToString() throws Exception {
+        IntegerToStringConverter converter = new IntegerToStringConverter();
+        String result = converter.convert(10);
+        assertThat(result).isEqualTo("10");
+    }
+
+    @Test
+    @DisplayName("IpPort 객체를 문자열로 변환 컨버터")
+    void IpPortToString() throws Exception {
+        IpPortToStringConverter converter = new IpPortToStringConverter();
+        IpPort source = new IpPort("127.0.0.1", 8080);
+        String result = converter.convert(source);
+        assertThat(result).isEqualTo("127.0.0.1:8080");
+    }
+
+    @Test
+    @DisplayName("문자열을 IpPort 객체로 변환 컨버터")
+    void StringToIpPort() throws Exception {
+        StringToIpPortConverter converter = new StringToIpPortConverter();
+        String source = "127.0.0.1:8080";
+        IpPort result = converter.convert(source);
+        assertThat(result).isEqualTo(new IpPort("127.0.0.1", 8080));
+    }
+}
